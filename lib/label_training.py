ea4c797ee54c06014f4adbe9e208f30d07a9cbf3
calavori <calavori1998@gmail.com>
2019-09-25 10:47:25 +0700

    update face_model tranning

---
+++ lib/label_training.py
@@ -0,0 +1,50 @@
+import os
+from PIL import Image
+import numpy as np
+import cv2
+import pickle
+#The endswith() method returns True if the string ends with the specified value, otherwise False.
+class Label_training:
+  def train():
+    image_dir = "dataset/face_recognition"
+
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+    current_id = 0
+    label_ids = {}
+    y_labels = []
+    x_train = []
+
+    for root, dirs, files in os.walk(image_dir):
+        if (dirs != 'fake' and dir != 'real' ):
+            for file in files:
+                    if file.endswith("png") or file.endswith("jpg"):
+                        path = os.path.join(root, file)
+                        label = os.path.basename(root).replace(" ", "-").lower()
+                        print(label, path)
+                        if not label in label_ids:
+                            label_ids[label] = current_id
+                            current_id += 1
+                        id_ = label_ids[label]
+                        print(label_ids)
+                        #y_labels.append(label) #number
+                        #x_train.append(path) # verify image, turn Numy array
+                        pil_image = Image.open(path).convert("L") #grayscale
+                        size = (550, 550)
+                        final_image = pil_image.resize(size, Image.ANTIALIAS)            
+                        image_array = np.array(pil_image, "uint8")
+                        print(image_array)
+                        faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5) 
+
+                        for (x,y,w,h) in faces:
+                            roi = image_array[y:y+h, x:x+w]
+                            x_train.append(roi)
+                            y_labels.append(id_)
+    print(y_labels)
+    print(x_train)
+    with open("dataset/face_model/labels.pickle", 'wb') as f: #wb writing bytes, f file
+        pickle.dump(label_ids, f)
+
+    recognizer.train(x_train, np.array(y_labels))
+    recognizer.save("dataset/face_model/trainner.yml")
--- lib/test.py
+++ lib/test.py
@@ -1,10 +1,13 @@
 from add_data import Add_data
 from liveness_detector.liveness_training import Liveness_training
+from label_training import Label_training
+from face import Face
 
 class Test:
     print("Option:")
     print("1. Add new data")
     print("2. Train liveness detector")
+    print("3. Train face detector")
     option = input("Your option: ")
     if option == "1":
         name = input("Enter your name: ")
@@ -12,6 +15,10 @@ class Test:
     if option == "2":
         liveness = Liveness_training()
         liveness.train()
+    if option == "3":
+        Label_training.train()
+    if option == "4":
+        Face.check()
 
     
 
